4211ffa2f0d901ace85c0c0f7ebc943b48e52a93
Binaya Bhandari <binaybhandari517@gmail.com>
2025-05-31 07:56:33 +0545

    jsx completion

--- bmi_cal/src/App.jsx
+++ bmi_cal/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className="container">
+        <h2>BMI Calcuator</h2>
+        <form>
+          <div>
+            <label>Weight (lbs)</label>
+            <input type="text" placeholder="Enter weight value"></input>
+          </div>
+          <div>
+            <label>Height (in)</label>
+            <input type="text" placeholder="Enter height value"></input>
+          </div>
+          <div>
+            <button className="btn" type="submit">
+              submit
+            </button>
+            <button className="btn btn-outline" type="submit">
+              Reload
+            </button>
+          </div>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
